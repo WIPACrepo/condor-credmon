3dd3423019304b3df9d08aef55a2bd389033fdc4
David Schultz <david.schultz@icecube.wisc.edu>
2025-08-15 15:22:46 -0500

    fix casting to int for config

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,26 @@
-use log::{error, info};
 use memoize::memoize;
 use serde_json::{Map, Value};
 use std::process::Command;
+
+use crate::error::CredmonError;
 
 static HTCONDOR_CONFIG: [&str; 2] = ["-c", "import htcondor,json; print(json.dumps({k:v for k,v in htcondor.param.items()}))"];
 
 pub type Config = Map<String, Value>;
 
+pub fn coerce_to_int(val: &Value) -> Result<u64, Box<dyn std::error::Error>> {
+    match val.as_u64() {
+        Some(x) => Ok(x),
+        None => match val.as_str() {
+            Some(x) => Ok(x.parse::<u64>()?),
+            None => Err(Box::new(CredmonError::ConfigError("not an integer".into()))),
+        },
+    }
+}
+
 #[memoize]
 pub fn config() -> Config {
-    info!(target:"config", "Loading HTCondor config");
+    log::info!(target:"config", "Loading HTCondor config");
 
     // Execute the Python script
     let output = Command::new("python3").args(HTCONDOR_CONFIG).output().expect("Cannot get HTCondor config!");
@@ -17,7 +28,7 @@ pub fn config() -> Config {
     // Check if the command was successful
     if !output.status.success() {
         let err = String::from_utf8(output.stderr).expect("cannot decode stderr");
-        error!(target: "config", "Python script failed: {err}");
+        log::error!(target: "config", "Python script failed: {err}");
         panic!("Cannot get HTCondor config!");
     }
 
